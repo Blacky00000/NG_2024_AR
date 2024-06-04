a919fc180a2402ec2cc99f1ef320d6c1923fed2f
Lina Romanova <Linamaikens@gmail.com>
2024-06-04 21:07:01 +0200

    Added Task_4

---
+++ Lesson1/Task_4/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+    int num1, num2 = 0;
+    int number = 0;
+    //int operation;
+    char operation;
+
+    cout << "- Choose and enter the operator (+, -, *, /) \n";
+    cout << "- Choose more operators (m) ";
+    cout << "\nI choose: ";
+    cin >> operation;
+
+    if (operation == 'm'){
+        cout << "Choose an operator ( ^ (Square of ), s (Square root of ): ";
+        cin >> operation;
+        cout << "Write a number : " ;
+        cin >> number;
+
+        switch (operation) {
+        case 's':
+            cout << "√" << number << "=" << sqrt(number);
+            break;
+        case '^':
+            cout << number << "^2=" << number * number;
+            break;
+        }
+
+    } else{
+        cout << "Write a num1 : " ;
+        cin >> num1;
+
+        cout << "Write a num2 : ";
+        cin >> num2;
+
+        switch (operation){
+        case '+':
+            cout << num1 << "+" << num2 << "=" << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << "-" << num2 << "=" << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << "*" << num2 << "=" << num1 * num2;
+            break;
+        case '/':
+            if (num2!= 0)
+                cout << num1 << "/" << num2 << "=" << static_cast<double>(num1) / num2 << endl;
+            else
+                cout << "Cannot divide by zero." << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
