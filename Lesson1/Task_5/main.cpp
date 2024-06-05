2cca1279d76043b9a0e4643167f7419a304f299c
Lina Romanova <Linamaikens@gmail.com>
2024-06-05 19:39:06 +0200

    .gitignore fix try

---
+++ Lesson1/Task_5/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+
+    double a, b, c, D, x, x1, x2;
+
+    cout << "Solving the equation of the form - ax^2 + bx + c = 0 \n" ;
+    cout << "Write a : " ;
+    cin >> a;
+
+    cout << "Write b : ";
+    cin >> b;
+
+    cout << "Write c : ";
+    cin >> c;
+
+    cout << "Finding the discriminant...";
+
+    cout << "D = " << b << "^2 - " << "4 * " << a << " * " << c << " = " << pow(b, 2) - 4*a*c << endl;
+
+    D = pow(b, 2) - 4*a*c;
+
+    if (D == 0) {
+        x = -b / (2*a);
+        cout << "Since D is 0, the equation has a single root: \nx = -b / 2a " << x << endl;
+    } else if (D > 0) {
+        x1 = (-b + sqrt(D)) / (2*a);
+        x2 = (-b - sqrt(D)) / (2*a);
+        cout << "Since D is greater than 0, the equation has two roots: \nx1 = " << x1 << ", x2 = " << x2 << endl;
+    } else {
+        cout << "Since D is less than zero, the equation has no solutions. BUT – ";
+        cout << "\nYou can do it with the complex(imaginary) roots."  << endl;
+        cout << "x1 = " << -b/(2*a) << " + " << sqrt(-D)/(2*a) << "i" << endl;
+        cout << "x2 = " << -b/(2*a) << " - " << sqrt(-D)/(2*a) << "i" << endl;
+        cout << ", where i is an imaginary unit ( i = √-1 ) "  << endl;
+
+    }
+    return 0;
+}
---
+++ Lesson1/Task_6/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+    for (int row = 0; row < size; row++) {
+        for (int backspace = 0; backspace < size - row; backspace++) {
+            cout << " ";
+        }
+        cout << "*";
+        for (int index = 0; index < row * 2; index++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int backspace = 0; backspace < size; backspace++) {
+        cout << " ";
+    }
+    cout << "*" << endl;
+    system("pause");
+
+    return 0;
+
+}
+
