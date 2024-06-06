36b9fb05ffdb7e38ea3779889319dfe0930c684e
Lina Romanova <Linamaikens@gmail.com>
2024-06-06 20:27:08 +0200

    Changed Task_5

--- Lesson1/Task_5/main.cpp
+++ Lesson1/Task_5/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 
-    double a, b, c, D, x, x1, x2;
+    double a, b, c;
 
     cout << "Solving the equation of the form - ax^2 + bx + c = 0 \n" ;
     cout << "Write a : " ;
@@ -19,24 +19,27 @@ int main()
     cout << "Write c : ";
     cin >> c;
 
-    cout << "Finding the discriminant...";
+    cout << "Finding the discriminant..." << endl;
 
-    cout << "D = " << b << "^2 - " << "4 * " << a << " * " << c << " = " << pow(b, 2) - 4*a*c << endl;
+    double D = pow(b, 2) - 4*a*c;
 
-    D = pow(b, 2) - 4*a*c;
+    cout << "D = " << b << "^2 - " << "4 * " << a << " * " << c << " = " << D << endl;
+
 
     if (D == 0) {
-        x = -b / (2*a);
+        double x = -b / (2*a);
         cout << "Since D is 0, the equation has a single root: \nx = -b / 2a " << x << endl;
     } else if (D > 0) {
-        x1 = (-b + sqrt(D)) / (2*a);
-        x2 = (-b - sqrt(D)) / (2*a);
+        double x1 = (-b + sqrt(D)) / (2*a);
+        double x2 = (-b - sqrt(D)) / (2*a);
         cout << "Since D is greater than 0, the equation has two roots: \nx1 = " << x1 << ", x2 = " << x2 << endl;
     } else {
+        double realPart = -b / (2*a);
+        double imaginaryPart = sqrt(-D)/(2*a);
         cout << "Since D is less than zero, the equation has no solutions. BUT - ";
         cout << "\nYou can do it with the complex(imaginary) roots."  << endl;
-        cout << "x1 = " << -b/(2*a) << " + " << sqrt(-D)/(2*a) << "i" << endl;
-        cout << "x2 = " << -b/(2*a) << " - " << sqrt(-D)/(2*a) << "i" << endl;
+        cout << "x1 = " << realPart << " + " << imaginaryPart << "i" << endl;
+        cout << "x2 = " << realPart << " - " << imaginaryPart << "i" << endl;
         cout << ", where i is an imaginary unit ( i = √-1 ) "  << endl;
 
     }
